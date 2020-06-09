39ae32f986247bdad623a8c641d7cf9bafdc5603
qmx769 <qiumx135>
2020-06-09 10:25:21 +0800

    添加弹射动画实体类

---
+++ app/src/main/java/com/kks/compx202_assignment8/Brick.java
@@ -0,0 +1,19 @@
+package com.kks.compx202_assignment8;
+
+/**
+ * Created by entity
+ */
+
+public class Brick {
+    public Brick(int mBrickX1, int mBrickY1, int mBrickX2, int mBrickY2) {
+        this.mBrickX1 = mBrickX1;
+        this.mBrickY1 = mBrickY1;
+        this.mBrickX2 = mBrickX2;
+        this.mBrickY2 = mBrickY2;
+    }
+
+    public int mBrickX1;
+    public int mBrickY1;
+    public int mBrickX2;
+    public int mBrickY2;
+}
